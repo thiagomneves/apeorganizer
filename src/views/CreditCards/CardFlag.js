cc05f5e0b0bd8ff0e4b56d9e451d96152a79faa7
Thiago Neves <neves@bb.com.br>
2022-06-13 03:22:21 -0300

    CardFlag component

--- src/views/CreditCards/CardFlag.js
+++ src/views/CreditCards/CardFlag.js
@@ -1,29 +1,43 @@
 import React from 'react';
-import {Image, StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
-import MasterCard from '../../assets/images/mastercard.png';
-import Visa from '../../assets/images/visa.png';
-// import Visa from '../../assets/images/g12.png';
-
-import AmericanExpress from '../../assets/images/amex.png';
-// import AmericanExpress from '../../assets/images/g1442.png';
-import Elo from '../../assets/images/elo.png';
-// import Elo from '../../assets/images/g1479.png';
+import MasterCard from '../../assets/images/mastercard.svg';
+import Visa from '../../assets/images/visa.svg';
+import AmericanExpress from '../../assets/images/amex.svg';
+import Elo from '../../assets/images/elo.svg';
 
 export default function CardFlag({flag}) {
-  const flags = {
-    MasterCard: MasterCard,
-    Visa,
-    AmericanExpress,
-    Elo
-  };
 
-  return <Image style={estilo.flag} source={flags[flag]} />;
+  function FlagComponent({flag, width, height}) {
+    const newWidth = !!width ? width : 60
+    const newHeight = !!height ? height : 40
+
+    switch (flag) {
+      case 'MasterCard':
+        return <MasterCard width={newWidth} height={newHeight} />;
+      case 'Visa':
+        return <Visa width={newWidth} height={newHeight} />;
+      case 'AmericanExpress':
+        return <AmericanExpress width={newWidth} height={newHeight} />;
+      case 'Elo':
+        return <Elo width={newWidth} height={newHeight} />;
+      default:
+        return <></>;
+    }
+  }
+
+  return (
+    <View style={estilo.flag}>
+      <FlagComponent  flag={flag}/>
+    </View>
+  );
 }
 
 const estilo = StyleSheet.create({
   flag: {
-    width: '90%',
-    height: '80%',
-  },
-})
+    // width: '90%',
+    // height: '80%',
+    backgroundColor: '#eee',
+    borderRadius: 5,
+  }
+});
