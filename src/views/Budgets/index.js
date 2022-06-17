d2c17c0b67110d0b440a19852e9ee01aed6f8d53
Thiago Neves <neves@bb.com.br>
2022-06-17 07:15:12 -0300

    Budget

--- src/views/Budgets/index.js
+++ src/views/Budgets/index.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { ThemeContext } from '../../contexts/ThemeContext';
 
 import Budget from './Components/Budget';
@@ -9,6 +9,11 @@ export default function Budgets() {
   const estilo = estilos(chosenTheme)
   return (
     <View style={estilo.container}>
+      <Text>Orçamento geral > 100%</Text>
+      <Text>Meta R$2.000,00</Text>
+      <Text>Gasto R$2.140,00</Text>
+      <Text>Previsto R$2.140,00</Text>
+      <Text>Excedeu R$140,00</Text>
       <Budget title="Alimentação" budget="700" spent="250" color="red"/>
       <Budget title="Transporte" budget="300" spent="250" color="orange"/>
     </View>
