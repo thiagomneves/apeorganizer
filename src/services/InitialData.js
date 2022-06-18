df7f0f2614632b0729516fc6d4a21a7f4dd884cd
Thiago Neves <neves@bb.com.br>
2022-06-18 18:51:43 -0300

    Fixed initialCategories. It was inverted

--- src/services/InitialData.js
+++ src/services/InitialData.js
@@ -6,18 +6,19 @@ export default function initialData() {
 
 function initialCategories() {
   let categories = [
-    {title: "Alimentação", color: "#E74C3C", type: "revenue"},
-    {title: "Educação", color: "#27AE60", type: "revenue"},
-    {title: "Lazer", color: "#1ABC9C", type: "revenue"},
-    {title: "Moradia", color: "#9B59B6", type: "revenue"},
-    {title: "Pagamentos", color: "#212121", type: "revenue"},
-    {title: "Roupas", color: "#3498DB", type: "revenue"},
-    {title: "Saúde", color: "#EC407A", type: "revenue"},
-    {title: "Transporte", color: "#F1C40F", type: "revenue"},
-    {title: "Investimentos", color: "#0669CC", type: "expense"},
-    {title: "Outros", color: "#621E6B", type: "expense"},
-    {title: "Presentes", color: "#20BCC9", type: "expense"},
-    {title: "Salário", color: "#27AE60", type: "expense"},
+    {title: "Alimentação", color: "#E74C3C", type: "expense"},
+    {title: "Educação", color: "#27AE60", type: "expense"},
+    {title: "Lazer", color: "#1ABC9C", type: "expense"},
+    {title: "Moradia", color: "#9B59B6", type: "expense"},
+    {title: "Pagamentos", color: "#212121", type: "expense"},
+    {title: "Roupas", color: "#3498DB", type: "expense"},
+    {title: "Saúde", color: "#EC407A", type: "expense"},
+    {title: "Transporte", color: "#F1C40F", type: "expense"},
+    
+    {title: "Investimentos", color: "#0669CC", type: "revenue"},
+    {title: "Outros", color: "#621E6B", type: "revenue"},
+    {title: "Presentes", color: "#20BCC9", type: "revenue"},
+    {title: "Salário", color: "#27AE60", type: "revenue"},
   ];
 
   categories.forEach(category => addCategory(category));
