b34f4a782a111e341f7d70af644debd12a72eb66
Thiago Neves <neves@bb.com.br>
2022-06-20 01:36:25 -0300

    Created Card component

---
+++ src/views/CreditCards/Components/Card.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import CardFlag from './CardFlag';
+
+export default function Card(props) {
+  const {color, flag} = props;
+  const name = "Thiago M Neves"
+
+  const estilo = estilos(color)
+  return (
+    <View style={estilo.cardContainer}>
+      <View style={estilo.cardContent}>
+        <MaterialCommunityIcons style={estilo.chip} name="integrated-circuit-chip"/>
+        <View style={estilo.flagContainer}><CardFlag flag={flag} width={70} noBg={true}/></View>
+        <Text style={estilo.client}>{name.toUpperCase()}</Text>
+      </View>
+    </View>
+  );
+}
+
+const estilos = color => {
+  return StyleSheet.create({
+    cardContainer: {
+      padding: 20,
+    },
+    cardContent: {
+      backgroundColor: color,
+      height: 200,
+      borderRadius: 10,
+    },
+    flagContainer: {
+      position: 'absolute',
+      right: 10,
+      bottom: 10,
+    },
+    client: {
+      fontSize: 18,
+      color: '#fff',
+      fontWeight: '500',
+      position: 'absolute',
+      bottom: 10,
+      left: 30,
+    },
+    chip: {
+      color: '#fff',
+      fontSize: 50,
+      position: 'absolute',
+      top: 50,
+      left: 30,
+    }
+  });
+};
--- src/views/CreditCards/Components/CardFlag.js
+++ src/views/CreditCards/Components/CardFlag.js
@@ -9,41 +9,42 @@ import AmericanExpress from '../../../assets/images/amex.svg';
 import Elo from '../../../assets/images/elo.svg';
 import CreditCard from '../../../assets/images/creditcard.svg';
 
-export default function CardFlag({flag, width}) {
+export default function CardFlag({flag = 'Outro', width, noBg = false}) {
   const newWidth = width ? width : 50;
   const newHeight = (newWidth * 4) / 6;
   const {chosenTheme} = useContext(ThemeContext);
-  const estilo = estilos({theme: chosenTheme,width: newWidth, height: newHeight});
+  const estilo = estilos({theme: chosenTheme,width: newWidth, height: newHeight, noBg});
 
-  function FlagComponent({flag = 'Outro', width, height}) {
+  function FlagComponent({flag , width, height, noBg }) {
 
     switch (flag) {
       case 'MasterCard':
-        return <MasterCard width={width} height={height} />;
+        return <MasterCard width={width} height={height} noBg={noBg}/>;
       case 'Visa':
-        return <Visa width={width} height={height} />;
+        return <Visa width={width} height={height} noBg={noBg}/>;
       case 'AmericanExpress':
-        return <AmericanExpress width={width} height={height} />;
+        return <AmericanExpress width={width} height={height} noBg={noBg}/>;
       case 'Elo':
-        return <Elo width={width} height={height} />;
+        return <Elo width={width} height={height} noBg={noBg}/>;
       default:
-        return <CreditCard width={width} height={height} />;
+        return <CreditCard width={width} height={height} noBg={noBg}/>;
     }
   }
 
   return (
     <View style={estilo.flag}>
-      <FlagComponent flag={flag} width={newWidth} height={newHeight}/>
+      <FlagComponent flag={flag} width={newWidth} height={newHeight} noBg={noBg}/>
     </View>
   );
 }
 
-const estilos = ({theme, width, height}) => {
+const estilos = ({theme, width, height, noBg}) => {
+
   return StyleSheet.create({
     flag: {
       width: width,
       height: height,
-      backgroundColor: theme.flag,
+      backgroundColor: noBg ? false : theme.flag,
       borderRadius: 5,
     },
   });
