13bbcb52f9cb258f7a3b943243fa483532d2e07b
Thiago Neves <neves@bb.com.br>
2022-06-26 02:00:58 -0300

    Created Global states for destroy and archive

--- src/contexts/GlobalContext.js
+++ src/contexts/GlobalContext.js
@@ -4,12 +4,18 @@ export const GlobalContext = createContext({});
 
 export function GlobalProvider({children}) {
   const [save, setSave] = useState(false);
+  const [destroy, setDestroy] = useState(false);
+  const [archive, setArchive] = useState(false);
 
   return (
     <GlobalContext.Provider
       value={{
         save,
         setSave,
+        destroy, 
+        setDestroy,
+        archive,
+        setArchive,
       }}>
       {children}
     </GlobalContext.Provider>
